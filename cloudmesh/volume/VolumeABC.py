9ceeb7e61f565b033332c86772a4688b90867e11
ashleysathornton <59942848+ashleysathornton@users.noreply.github.com>
2020-02-19 20:10:33 -0700

    Update VolumeABC.py

--- cloudmesh/volume/VolumeABC.py
+++ cloudmesh/volume/VolumeABC.py
@@ -1,1 +1,10 @@
+class volume()
 
+            #volume create [name]
+                  #[--size SIZE]
+                  #[--type VOLUME-TYPE]
+                  #[--image IMAGE | --snapshot SNAPSHOT | --source VOLUME]
+                  #[--description DESCRIPTION]
+
+            def create(name=name, size=size, voltype=voltype, image=image, snapshot=snapshot, source=source, description=description):
+              raise not implemented error

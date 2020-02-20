2fcd14b4fb332c4511abda2714a7e42a651818f8
ashleysathornton <59942848+ashleysathornton@users.noreply.github.com>
2020-02-19 20:26:17 -0700

    Update Volume.py

--- cloudmesh/volume/Volume.py
+++ cloudmesh/volume/Volume.py
@@ -1,1 +1,3 @@
 
+#Gregor will help us, can only do this after the abstract base class is defined
+#Example can find under cloudmesh-cloud/cloudmesh/compute/*, cloudmesh-cloud/cloudmesh/vm/command, cloudmesh-storage/cloudmesh/storage/*

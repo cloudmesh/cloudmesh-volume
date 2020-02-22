8a4ee3e06411a8ff0e92f974826761b44b37f298
ashok2017 <ashok.singam@gmail.com>
2020-02-21 22:55:21 -0800

    Creating manual page for Create and Delete

--- cloudmesh/volume/command/volume.py
+++ cloudmesh/volume/command/volume.py
@@ -4,6 +4,7 @@ from cloudmesh.shell.command import PluginCommand
 from cloudmesh.common.console import Console
 from cloudmesh.shell.command import map_parameters
 from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.util import banner
 
 # from cloudmesh.volume.openstack.Provider import Provider
 
@@ -16,13 +17,15 @@ class VolumeCommand(PluginCommand):
         ::
 
           Usage:
-                volume create [NAME]
-                  [--size=SIZE]
-                  [--type=VOLUME-TYPE]
-                  [--image=IMAGE | --snapshot=SNAPSHOT | --source =VOLUME]
-                  [--description=DESCRIPTION]
+                volume create NAME
+                              [--size=SIZE]
+                              [--type=VOLUME-TYPE]
+                              [--image=IMAGE | --snapshot=SNAPSHOT | --source =VOLUME]
+                              [--description=DESCRIPTION]
+                              [--dryrun]
+                volume delete NAME [--dryrun]
 
-          This command does some useful things.
+          A simple abstraction layer to manage Cloud Volumes for AWS, Azure, Google, Openstack and Multipass
 
           Arguments:
               NAME   volume name
@@ -36,6 +39,8 @@ class VolumeCommand(PluginCommand):
           Commands:
             Create volume
               cms volume create NAME
+              Optionally you can provide size, volume type, image or description
+            Delete volume NAME
 
         """
 
@@ -47,25 +52,26 @@ class VolumeCommand(PluginCommand):
                        "type",
                        "image",
                        "descripton",
-                       "mem"
                        )
 
         VERBOSE(arguments)
 
         if arguments.create:
-            #volume create [name]
-                  #[--size SIZE]
-                  #[--type VOLUME-TYPE]
-                  #[--image IMAGE | --snapshot SNAPSHOT | --source VOLUME]
-                  #[--description DESCRIPTION]
-            print("create volume")
-            # provider = Provider()
-            size = arguments["--size"]
-            volumetype = arguments["--type"]
-            #provider.create(name=name, size=size, voltype=voltype, image=image, snapshot=snapshot, source=source, description=description)
-            
-        elif arguments.list:
-            print("option b")
+            if arguments.dryrun:
+                banner("dryrun create")
+            else:
+                size = arguments["--size"]
+                volumetype = arguments["--type"]
+                #provider = Provider()
+                #provider.create(name=name, size=size, voltype=voltype, image=image, snapshot=snapshot, source=source, description=description)
+                print("create volume is not yet implemented!")
+        elif arguments.delete:
+            if arguments.dryrun:
+                banner("dryrun delete")
+            else:
+                # provider = Provider()
+                # provider.delete(name=name)
+                print("delete volume is not yet implemented!")
 
         Console.error("This is just a sample")
         return ""

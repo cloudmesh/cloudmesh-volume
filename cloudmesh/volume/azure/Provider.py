ae3149bf57d0d35eade941a1c5670276ddceb911
Ashley Thornton <59942848+ashleysathornton@users.noreply.github.com>
2020-03-09 19:46:14 -0700

    Update Provider.py

--- cloudmesh/volume/azure/Provider.py
+++ cloudmesh/volume/azure/Provider.py
@@ -66,19 +66,19 @@ class Provider(ComputeNodeABC, ComputeProviderPlugin):
                 version: latest
                 service: compute
               default:
-                image: Canonical:UbuntuServer:16.04.0-LTS:latest
-                size: Basic_A0
-                resource_group: cloudmesh
-                storage_account: cmdrive
-                network: cmnetwork
-                subnet: cmsubnet
-                blob_container: vhds
-                AZURE_VM_IP_CONFIG: cloudmesh-ip-config
-                AZURE_VM_NIC: cloudmesh-nic
-                AZURE_VM_DISK_NAME: cloudmesh-os-disk
-                AZURE_VM_USER: TBD
-                AZURE_VM_PASSWORD: TBD
-                AZURE_VM_NAME: cloudmeshVM
+#               image: Canonical:UbuntuServer:16.04.0-LTS:latest
+#               size: Basic_A0
+#               resource_group: cloudmesh
+#               storage_account: cmdrive
+#               network: cmnetwork
+#               subnet: cmsubnet
+#               blob_container: vhds
+#               AZURE_VM_IP_CONFIG: cloudmesh-ip-config
+#               AZURE_VM_NIC: cloudmesh-nic
+#               AZURE_VM_DISK_NAME: cloudmesh-os-disk
+#               AZURE_VM_USER: TBD
+#               AZURE_VM_PASSWORD: TBD
+#               AZURE_VM_NAME: cloudmeshVM
               credentials:
                 AZURE_TENANT_ID: {tenantid}
                 AZURE_SUBSCRIPTION_ID: {subscriptionid}

30850ee44926bfe35ea925a2b541b94ee6a9d5de
Gregor von Laszewski <laszewski@gmail.com>
2020-03-10 06:04:12 -0400

    add a provider get method

--- cloudmesh/volume/Volume.py
+++ cloudmesh/volume/Volume.py
@@ -22,7 +22,7 @@ class Provider(VolumeABC):
         self.cloud = name
         #cred = self.spec["credentials"]
         #deft = self.spec["default"]
-        #self.cloudtype = self.spec["cm"]["kind"]
+        self.kind = self.spec["cm"]["kind"]
         super().__init__(name, conf)
 
         print(self.cloud)
@@ -31,14 +31,49 @@ class Provider(VolumeABC):
         #
         # BUG: the test must be self.kind and not self.cloud
         #
-        if self.cloud == "multipass":
+
+        self.provider = self.get(self.kind)
+
+    def get(self, kind):
+
+        if self.kind == "multipass":
             from cloudmesh.volume.multipass.Provider import \
                 Provider as MulitpassProvider
             self.provider = MulitpassProvider(self.cloud)
 
-    def create(self, name=None):
-        banner(f"mount {name}")
-        os.system(f"multipass mount /Users/ashok/multipass-mount  {name}")
+        elif self.kind == "aws":
+            from cloudmesh.volume.aws.Provider import \
+                Provider as AwsProvider
+            self.provider = AwsProvider(self.cloud) #
+
+        return self.provider
+
+
+    def create(self, name=None):  #, **args):
+        #banner(f"mount {name}")
+        # #os.system(f"multipass mount /Users/ashok/multipass-mount  {name}")
+
+        self.provider.create(name)
+        """
+        name = args["name"]
+        def create(self,
+                   name=None,
+                   zone=None,
+                   size=None,
+                   voltype="gp2",
+                   iops=1000,
+                   kms_key_id=None,
+                   outpost_arn=None,
+                   image=None,
+                   snapshot=None,
+                   encrypted=False,
+                   source=None,
+                   description=None,
+                   tag_key=None,
+                   tag_value=None,
+                   multi_attach_enabled=True,
+                   dryrun=False):
+        """
 
     def mount(self, path=None,name=None):
         self.provider.mount(path,name)

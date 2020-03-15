b8b3d29db6216cbf6d9a11ac08f1ee1a10e12e0d
Ashley Thornton <ashleysathornton@gmail.com>
2020-03-14 18:25:02 -0700

    corrected authentication file

--- cloudmesh/volume/azure/Provider.py
+++ cloudmesh/volume/azure/Provider.py
@@ -26,7 +26,7 @@ CLOUDMESH_YAML_PATH = "~/.cloudmesh/cloudmesh.yaml"
 from azure.common.client_factory import get_client_from_auth_file
 from azure.mgmt.compute import ComputeManagementClient
 
-client = get_client_from_auth_file(ComputeManagementClient, auth_path=C:\Users\plj2861\Documents\Ashley Personal\School\Indiana University\Cloud Computing\azure_credentials.json)
+client = get_client_from_auth_file(ComputeManagementClient, auth_path=C:\Users\plj2861\Documents\AshleyPersonal\School\IndianaUniversity\CloudComputing\azure_credentials.json)
 
 
 def _remove_mongo_id_obj(dict_list):

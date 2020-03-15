5424efc3db390a3cdb59c2f4ce82628eafb29c82
mccandpm <54606758+mccandpm@users.noreply.github.com>
2020-03-15 15:29:32 -0400

    Removed CLI commands from google Provider.py, need to add googleapiclient compute v1 commands using googleapiclient.discovery

--- cloudmesh/volume/google/Provider.py
+++ cloudmesh/volume/google/Provider.py
@@ -1,13 +1,11 @@
-#
-# class Provider(VolumeABC):
-#
-#   def list( .... ):
-#      raise ImpelentainError
-
+import os
+import json
 from cloudmesh.volume.VolumeABC import VolumeABC
 from cloudmesh.common.util import banner
 from cloudmesh.common.Shell import Shell
 from cloudmesh.configuration.Config import Config
+from datetime import datetime
+import googleapiclient.discovery
 
 
 class Provider(VolumeABC):
@@ -62,139 +60,45 @@ class Provider(VolumeABC):
     def __init__(self, name):
         self.cloud = name
 
-    def create(self,
-               name=None,
-               csek_key_file=None,
-               description=None,
-               guest_os_features=None,
-               labels=None,
-               licenses=None,
-               replica_zones=None,
-               resources_policies=None,
-               require_csek_key=False,
-               size=None,
-               type=None,
-               image_project=None,
-               image=None,
-               image_family=None,
-               source_snapshot=None,
-               kms_key=None,
-               kms_keyring=None,
-               kms_location=None,
-               kms_project=None,
-               region=None,
-               zone=None):
+    def update_dict(self, elements):
         """
-        Create a disk.
+        This function adds a cloudmesh cm dict to each dict in the list
+        elements.
+        Libcloud
+        returns an object or list of objects With the dict method
+        this object is converted to a dict. Typically this method is used
+        internally.
 
-        :param name: Names of the disks to create
-        :param csek_key_file: Path to a Customer-Supplied Encryption Key (CSEK)
-        :param description: An optional, textual description for the disks
-                            being created.
-        :param guest_os_features: This parameter enables one or more features
-                                    for VM instances that use the image for
-                                    their boot disks. Must be one of:
-                                    MULTI_IP_SUBNET, SECURE_BOOT,
-                                    UEFI_COMPATIBLE, VIRTIO_SCSI_MULTIQUEUE,
-                                    WINDOWS
-        :param labels: List of label key-value pairs
-        :param liscences: List of URI's to licence resources
-        :param replica_zones: A comma-separated list of exactly 2 zones that a
-                                regional disk will be replicated to.
-        :param require_csek_key: Refuse to create resources not protected by
-                                    a user managed key in the key file
-        :param resoucrces_policies: A list of resource policy names to be
-                                    added to the disk
-        :param size: Size of the disk. Whole number followed by 'GB' or 'TB'
-                                for gigabyte or terabyte
-        :param type: Type of the disk
-        :param image_project: Project against which all image and image family
-                                references will be resolved.
-        :param image: An image to apply to the disks being created.
-        :param image_family: The image family for the operating system that
-                                the boot disk will be initialized with.
-        :param source_snapshot: Source snapshot used to create the disks.
-        :param kms_key: ID of the key or fully qualified identifier for the key.
-        :param kms_keyring: The KMS keyring of the key
-        :param kms_project: The cloud project for the key
-        :param region: Region of the disks to create.
-        :param zone: Zone of the disks to create.
-        :return:
+        :param elements: the list of original dicts. If elements is a single
+                         dict a list with a single element is returned.
+        :param kind: for some kinds special attributes are added. This includes
+                     key, vm, image, flavor.
+        :return: The list with the modified dicts
         """
-        raise NotImplementedError
 
-    def delete(self,
-               name=None,
-               region=None,
-               zone=None):
-        """
-        Delete a disk
+        if elements is None:
+            return None
+        elif type(elements) == list:
+            _elements = elements
+        else:
+            _elements = [elements]
+        d = []
+        for entry in _elements:
 
-        :param name: Name of the disk
-        :param region: Region of the disk
-        :param zone: Zone of the disk
-        :return:
-        """
-        raise NotImplementedError
+            if "cm" not in entry:
+                entry['cm'] = {}
 
-    def describe(self,
-                 name=None,
-                 region=None,
-                 zone=None):
-        """
-        Displays all data associated with a disk in a project.
+            entry["cm"].update({
+                "kind": "volume",
+                "driver": self.cloudtype,
+                "cloud": self.cloud,
+                "name": self.name
+            })
 
-        :param name: Name of the disk
-        :param region: Region of the disk
-        :param zone: Zone of the disk
-        :return:
-        """
-        raise NotImplementedError
+            entry["cm"]["created"] = entry["updated"] = str(
+                datetime.now())
 
-    def list(self,
-             name=None,
-             regexp=None,
-             regions=None,
-             zones=None,
-             filter=None,
-             limit=None,
-             page_size=None,
-             sort_by=None,
-             uri=None):
-        """
-        Displays all disks in a project
+            d.append(entry)
+        return d
 
-        :param name: (DEPRECATED) If provided, show details for the specified
-                        names and/or URIs of resources.
-        :param regexp: (DEPRECATED) Regular expression to filter the names of
-                        the results on. Any names that do not match the entire
-                        regular expression will be filtered out.
-        :param regions: (DEPRECATED) If provided, only regional resources
-                        are shown.
-        :param zones: (DEPRECATED) If provided, only zonal resources are shown.
-        :param filter: Apply a Boolean filter to each resource item to
-                        be listed.
-        :param limit: Specifies the maximum number of resources to list.
-        :param page_size:  Specifies the maximum number of resources per page.
-        :param sort_by: Comma-separated list of resource field key names
-                        to sort by.
-        :param uri: Print a list of resource URIs instead of the default output.
-        :return:
-        """
-        raise NotImplementedError
 
-    def move(self,
-             name=None,
-             destination_zone=None,
-             a_sync=False,
-             zone=None):
-        """
-        Move a disk from one zone to another.
-
-        :param name:
-        :param destination_zone:
-        :param async:
-        :param zone:
-        :return:
-        """
-        raise NotImplementedError

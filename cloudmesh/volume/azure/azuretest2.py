d77a6244e67d94e194c938bdf98268cd27e0f99c
AshleyThornton <ashthorn@iu.edu>
2020-03-27 10:34:02 -0700

    adds

--- cloudmesh/volume/azure/azuretest2.py
+++ cloudmesh/volume/azure/azuretest2.py
@@ -12,3 +12,44 @@ p.provider
 print(p.create_dir())
 
 print(p.list())
+
+
+
+
+
+# # oracle list
+#
+# def list(self, **kwargs):
+#     if kwargs["--refresh"]:
+#         block_storage = oci.core.BlockstorageClient(self.config)
+#         v = block_storage.list_volumes(self.config['compartment_id'])
+#         results = v.data
+#         result = self.update_dict(results)
+#         print(self.Print(result, kind='volume', output=kwargs['output']))
+#     else:
+#         # read record from mongoDB
+#         refresh = False
+#
+#
+# # openstack list
+#
+#     def list(self,**kwargs):
+#         if kwargs["--refresh"]:
+#             con = openstack.connect(**self.config)
+#             results = con.list_volumes()
+#             result = self.update_dict(results)
+#             print(self.Print(result, kind='volume', output=kwargs['output']))
+#         else:
+#             # read record from mongoDB
+#             refresh = False
+#
+#
+# # aws list
+#
+#     def list(self,**kwargs):
+#         client = boto3.client('ec2')
+#         dryrun = kwargs['--dryrun']
+#         result = client.describe_volumes(
+#             DryRun=dryrun,
+#         result = self.update_dict(result)
+#         return result

bdce23fa2550e7726e751cdbe074648eb31ec372
AshleyThornton <ashthorn@iu.edu>
2020-03-26 18:01:55 -0700

    adds

--- cloudmesh/volume/azure/azuretest2.py
+++ cloudmesh/volume/azure/azuretest2.py
@@ -2,9 +2,12 @@
 import cloudmesh.volume.provider.azureblob.Provider as Provider
 
 p = Provider(service="azureblob", config='path to config')
+cmp = cmProvider(service="azureblob", config='path to config')
 
 #p.storage.service
 p.volume.service
+
+p.provider
 
 print(p.create_dir())
 

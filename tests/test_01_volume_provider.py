ce1dfd91557352c9ce8eb6c10ae6820192d19f1e
ashok2017 <ashok.singam@gmail.com>
2020-04-16 07:54:11 -0700

    Moved starting of vm an terminating of vms into separate test files

--- tests/test_01_volume_provider.py
+++ tests/test_01_volume_provider.py
@@ -1,24 +1,22 @@
 ###############################################################
-# pytest -v --capture=no tests/test_volume_openstack.py
+# pytest -v --capture=no tests/test_01_volume_provider.py
 ###############################################################
 
 # TODO: start this with cloud init, e.g, empty mongodb
 # TODO: assertuons need to be added
-from pprint import pprint
 
-import pytest
 import os
 from time import sleep
+
+import pytest
+from cloudmesh.common.Benchmark import Benchmark
 from cloudmesh.common.debug import VERBOSE
 from cloudmesh.common.util import HEADING
 from cloudmesh.common.variables import Variables
-from cloudmesh.common.Benchmark import Benchmark
-from cloudmesh.volume.Provider import Provider
 from cloudmesh.configuration.Config import Config
+from cloudmesh.volume.Provider import Provider
+
 from cloudmesh.management.configuration.name import Name
-from cloudmesh.mongo.CmDatabase import CmDatabase
-from cloudmesh.common.Shell import Shell
-import sys
 
 Benchmark.debug()
 
@@ -43,38 +41,22 @@ name_generator.set(f"test-{user}-volume-" + "{counter}")
 name = str(name_generator)
 provider = Provider(name=cloud)
 
-def Print(data):
-    print(provider.Print(data=data, output='table', kind='vm'))
 
 @pytest.mark.incremental
 class Test_provider_volume:
-
-    def test_cms_init(self):
-        HEADING()
-        Benchmark.Start()
-        result = os.system(f"cms init")
-        Benchmark.Stop()
-
-    def test_cms_vm(self):
-        HEADING()
-        cmd = "cms vm boot"+" --name="+name
-        Benchmark.Start()
-        result = Shell.run(cmd)
-        Benchmark.Stop()
-        print(result)
-        assert "vm" in result
 
     def test_provider_volume_create(self):
         HEADING()
         os.system(f"cms volume list --cloud={cloud}")
         name_generator.incr()
         Benchmark.Start()
-        params = {"NAME":name}
+        params = {"NAME": name}
         data = provider.create(**params)
         Benchmark.Stop()
+        status = None
         for v in data:
             status = v['status']
-        assert status in ['available','STARTING', 'RUNNING','ACTIVE']
+        assert status in ['available', 'STARTING', 'RUNNING', 'ACTIVE']
 
     def test_provider_volume_list(self):
         HEADING()
@@ -88,11 +70,10 @@ class Test_provider_volume:
         # test attach one volume to vm
         # os.system("cms volume attach {name} --vm={vm}")
         HEADING()
-        vm = variables['vm']
         Benchmark.Start()
         NAMES = []
         NAMES.append(name)
-        provider.attach(NAMES=NAMES,vm=name)
+        provider.attach(NAMES=NAMES, vm=variables.__getitem__("vm_name"))
         Benchmark.Stop()
         start_timeout = 360
         time = 0
@@ -100,10 +81,10 @@ class Test_provider_volume:
             sleep(5)
             time += 5
             status = provider.status(NAME=NAMES[0])[0]['status']
-            #In case of Oracle, status is AVAILABLE after attach
-            if status in ['in-use','AVAILABLE']:
+            # In case of Oracle, status is AVAILABLE after attach
+            if status in ['in-use', 'AVAILABLE']:
                 break
-        assert status in ['in-use','AVAILABLE']
+        assert status in ['in-use', 'AVAILABLE']
 
     def test_provider_volume_detach(self):
         # test detach one volume
@@ -118,9 +99,9 @@ class Test_provider_volume:
             sleep(5)
             time += 5
             status = provider.status(NAME=name)[0]['status']
-            if status in ['available','AVAILABLE']:
+            if status in ['available', 'AVAILABLE']:
                 break
-        assert status in ['available','AVAILABLE']
+        assert status in ['available', 'AVAILABLE']
 
     def test_provider_volume_delete(self):
         HEADING()
@@ -130,15 +111,5 @@ class Test_provider_volume:
         result = provider.info(name=name)
         assert result is None
 
-    def test_cms_terminate(self):
-        HEADING()
-        cmd = "cms vm terminate"
-        Benchmark.Start()
-        result = Shell.run(cmd)
-        Benchmark.Stop()
-        print(result)
-        assert "vm" in result
-
     def test_benchmark(self):
         Benchmark.print(sysinfo=False, csv=True, tag=cloud)
-
